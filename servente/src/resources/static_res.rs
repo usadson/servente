2f97a6fc1da79551b47cbe258c507d8fe30eac94
Tristan <code@thewoosh.org>
2023-03-26 15:44:45 +0200

    [resources] Fix resources directory `include!()` since the path has changed

--- servente/src/resources/static_res.rs
+++ servente/src/resources/static_res.rs
@@ -1,14 +1,23 @@
 // Copyright (C) 2023 Tristan Gerritsen <tristan@thewoosh.org>
 // All Rights Reserved.
-//
-// This file contains various static resources that are used by the
-// application. These resources are embedded into the binary using the
-// `include!` macros.
+
+//! This file contains various static resources that are used by the
+//! application. These resources are embedded into the binary using the
+//! `include!` macros.
+
+/// The path to the `/resources` directory in the repository root.
+///
+/// There isn't an environment variable like `CARGO_WORKSPACE_DIR` yet, so
+/// we resort to using relative paths instead.
+///
+/// `CARGO_MANIFEST_DIR` will return `[repository]/servente/` so `..` will
+/// get the workspace directory.
+const RESOURCES_DIRECTORY &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../resources/");
 
 /// The HTML page that is shown when the user visits the root of the
 /// application, without having overridden the default welcome page.
-pub const WELCOME_HTML: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/resources/welcome.html"));
+pub const WELCOME_HTML: &str = include_str!(concat!(RESOURCES_DIRECTORY, "welcome.html"));
 
 /// The HTML page that is shown when the user visits the root of the
 /// application, without having overridden the default welcome page.
-pub const WELCOME_HTML_NL: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/resources/welcome.nl.html"));
+pub const WELCOME_HTML_NL: &str = include_str!(concat!(RESOURCES_DIRECTORY, "welcome.nl.html"));
